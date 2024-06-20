e4e6cded60e74f54b10019ea2006059e1f03f2e7
SunilS97 <111933521+SunilS97@users.noreply.github.com>
2024-06-19 17:48:10 -0700

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://zapidfooddel-jfy5.onrender.com";
+  const frontend_url = "https://zapid-food-del.onrender.com";
 
   try {
     const newOrder = new orderModel({
